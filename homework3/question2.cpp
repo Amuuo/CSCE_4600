d871baafcfd7964862dd0a1e329265242b3bd849
Adam Williams <adamthewilliams@dynamic-10-125-186-121.dynamic.unt.edu>
2019-04-02 13:32:31 -0500

    .

--- homework3/question2.cpp
+++ homework3/question2.cpp
@@ -30,10 +30,13 @@ int main() {
 
 	string string1(100000000, '1');
 
+	printf("\n>> Writing to file");
 	write_to_file(string1);
+	printf("\n>> Done writing text.txt");
 	in = new ifstream{"text.txt"};
 
 
+	printf("\n>> Initializing threads");
 	thread thread1{read_from_file1};
 	thread thread2{read_from_file2};
 
@@ -58,34 +61,34 @@ void read_from_file2(){
 
 	string s;
 	{
-		printf("\nThread 2 attempting to acquire lock1");
+		printf("\n>> Thread 2 attempting to acquire lock1");
 		lock_guard<mutex> lck{lock1};
-		printf("\nThread 2 acquired lock1");
+		printf("\n>> Thread 2 acquired lock1");
 		while(!in->eof()) {
-			printf("\nThread 2 attempting to acquire lock2");
+			printf("\n>> Thread 2 attempting to acquire lock2");
 			lock_guard<mutex> lck2{lock2};
-			printf("\nThread 2 acquired lock2");
+			printf("\n>> Thread 2 acquired lock2");
 			s.push_back(in->get());
-			printf("\nThread 2 releasing lock2");
+			printf("\n>> Thread 2 releasing lock2");
 		}
 	}
-	printf("\nThread 2 released lock1");
+	printf("\n>> Thread 2 released lock1");
 }
 
 /* deadlocking function for thread 1 */
 void read_from_file1() { 
 	string s;
 	{
-		printf("\nThread 1 attempting to acquire lock2");
+		printf("\n>> Thread 1 attempting to acquire lock2");
 		lock_guard<mutex>lck{lock2};
-		printf("\nThread 1 acquired lock2");
+		printf("\n>> Thread 1 acquired lock2");
 		while(!in->eof()) {
-			printf("\nThread 1 attempting to acquire lock1");
+			printf("\n>> Thread 1 attempting to acquire lock1");
 			lock_guard<mutex> lck2{lock1};
-			printf("\nThread 1 acquired lock1");
+			printf("\n>> Thread 1 acquired lock1");
 			s.push_back(in->get());
-			printf("\nThread 1 releasing lock1");
+			printf("\n>> Thread 1 releasing lock1");
 		}
 	}
-	printf("\nThead 1 released lock2");
+	printf("\n>> Thread 1 released lock2");
 }
